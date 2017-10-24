02ba39eab826e90b6653ead1cd1d4a5497e8a641
Fabian Guist <fg-141605@HSWGT.HS-WEINGARTEN.DE>
2017-10-24 12:42:31 +0200

    Add project files.

---
+++ bombs_away/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bombs_away
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new GameWindow();
+            window.RenderFrame += Window_RenderFrame;
+            window.RenderFrame += (s, e) => window.SwapBuffers();
+            window.Run();
+        }
+
+        private static void Window_RenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+        }
+    }
+}
