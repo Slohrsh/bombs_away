cc9415554e58c12f7ee222cf3203fca6999b7a11
Slohrshi <sebastian.lohr@hs-weingarten.de>
2017-11-28 14:40:27 +0100

    Revert "tiled tmx parser half implemented"
    This reverts commit 93d6453681f2afa2c5be6337758fdb451dca673b.

--- bombs_away/LevelLoader.cs
+++ bombs_away/LevelLoader.cs
@@ -13,18 +13,13 @@ using bombs_away.game;
 using bombs_away.ui.elements.portal;
 using bombs_away.ui.elements.ground;
 using System.IO;
-using System.Xml;
-using Microsoft.VisualBasic.FileIO;
 
 namespace bombs_away
 {
     class LevelLoader
     {
         private float squareSize;
-        private XmlDocument doc;
-        private int mapWidth;
-        private int mapHeight;
-        /*public Level Load()
+        public Level Load()
         {
             int levelSize = CalculateAmountOfBlocksInXDirection();
             squareSize = CalculateSquareSize(levelSize);
@@ -51,87 +46,18 @@ namespace bombs_away
             }
 
             return new Level(grid, interactiveObjects);
-        }*/
-
-        public Level Load()
-        {
-
-            doc = new XmlDocument();
-            doc.Load("..\\..\\resources\\game\\map\\BasicMap.tmx");
-            setMapDimensions();
-
-            squareSize = CalculateSquareSize(mapWidth);
-
-            Block[,] grid = new Block[mapWidth, mapHeight];
-            List<Block> interactiveObjects = new List<Block>();
-            Stream stream = GenerateStreamFromString(getNodeValue("/map/layer/data"));
-            TextFieldParser parser = new TextFieldParser(stream);
-            parser.TextFieldType = FieldType.Delimited;
-            parser.SetDelimiters(",");        
-
-            for (int y = mapHeight - 1; y >= 0; y--)
-            {
-                string[] line = parser.ReadFields();
-                for (int x = mapWidth - 1; x >= 0; x--)
-                {
-                    if (isComponentStatic(line[x]))
-                    {
-                        grid[x, y] = LoadComponent(x, y, line[x]);
-                    }
-                    else
-                    {
-                        interactiveObjects.Add(LoadComponent(x, y, line[x]));
-                        //grid[x, y] = LoadComponent(x, y, '&');
-                    }
-                }
-            }
-
-            return new Level(grid, interactiveObjects); 
-
-            return null;
         }
 
-        public static Stream GenerateStreamFromString(string s)
-        {
-            MemoryStream stream = new MemoryStream();
-            StreamWriter writer = new StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
-
-
-
-        private void setMapDimensions()
-        {
-            XmlNode node = doc.DocumentElement.SelectSingleNode("/map");
-            mapWidth = getAttribute(node, "width");
-            mapHeight = getAttribute(node, "height");
-        }
-
-        private int getAttribute(XmlNode node, String attributeName)
-        {
-            String mapWidthString = node.Attributes[attributeName]?.InnerText;
-            return Int32.Parse(mapWidthString);
-        }
-
-        private String getNodeValue(String nodeName)
-        {
-            XmlNode node = doc.DocumentElement.SelectSingleNode(nodeName);
-            return node?.InnerText;
-        }
-
-        private bool isComponentStatic(String type)
+        private bool isComponentStatic(char type)
         {
             return type.Equals('&') || type.Equals('_');
         }
 
-        private Block LoadComponent(int gridX, int gridY, String type)
+        private Block LoadComponent(int gridX, int gridY, char type)
         {
             float x = TransformPositionRelative(gridX);
             float y = TransformPositionRelative(gridY);
-            /*switch (type)
+            switch (type)
             {
                 case '&': return new Block(BlockType.EMPTY, squareSize, x, y, false);
                 case 'E': return new Block(BlockType.ENEMY, squareSize, x, y);
@@ -139,8 +65,7 @@ namespace bombs_away
                 case 'P': return new Block(BlockType.PORTAL, squareSize, x, y, false);
                 case '_': return new Block(BlockType.GROUND, squareSize, x, y);
             }
-            return new Block(BlockType.EMPTY, squareSize, x, y, false);*/
-            return null;
+            return new Block(BlockType.EMPTY, squareSize, x, y, false);
         }
 
         private float TransformPositionRelative(int gridPosition)
@@ -153,6 +78,9 @@ namespace bombs_away
             return 1/levelSize;
         }
 
- 
+        private int CalculateAmountOfBlocksInXDirection()
+        {
+            return 10;
+        }
     }
 }
--- bombs_away/resources/game/map/NatureTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesetOpenGame" tilewidth="32" tileheight="32" tilecount="60" columns="10">
- <image source="tilesetOpenGame.png" width="320" height="192"/>
-</tileset>
