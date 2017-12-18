cd3eac46a6def71c1f9aa6591aeb463a68cc0779
fabianguist <fabian.guist@gmail.com>
2017-12-18 10:12:35 +0100

    Tilesets huhu

---
+++ bombs_away/resources/game/map/Tilesets/TilesetCharacters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TilesetCharacters" tilewidth="32" tileheight="32" tilecount="15" columns="3">
+ <image source="../TilesetImages/TilesetCharacters.png" width="120" height="160"/>
+</tileset>
